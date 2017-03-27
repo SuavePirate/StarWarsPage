7652d0b1fe2c2ff4178dbf3854c14312f059ab63
SuavePirate <alexander.dunn@outlook.com>
2017-03-27 11:58:23 -0400

    Created projects and page for scrolling

---
+++ src/StarWarsPage/StarWarsPage/MainPage.xaml.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+using Xamarin.Forms;
+
+namespace StarWarsPage
+{
+    public partial class MainPage : ContentPage
+    {
+        public MainPage()
+        {
+            InitializeComponent();
+        }
+    }
+}
